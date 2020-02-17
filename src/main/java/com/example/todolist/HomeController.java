5b4a33dc8d2798a787a495651807d4dd29dc19eb
GBTC440009UR <GBTC440009UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-17 16:36:03 -0500

    first commit

---
+++ src/main/java/com/example/todolist/HomeController.java
@@ -0,0 +1,52 @@
+package com.example.todolist;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
+@Controller
+public class HomeController {
+    @Autowired
+    ListRepository listRepository;
+
+    @RequestMapping("/")
+    public String list(Model model){
+        model.addAttribute("lists", listRepository.findAll());
+        return "list";
+    }
+    @GetMapping("/add")
+    public String listForm(Model model){
+        model.addAttribute("list", new List());
+        return "listform";
+    }
+    @PostMapping("/process")
+    public String processForm(@Valid List list, BindingResult result){
+        if(result.hasErrors()){
+            return "listform";
+        }
+        listRepository.save(list);
+        return "redirect:/";
+    }
+    @RequestMapping("/detail/{id}")
+    public String showCourse(@PathVariable("id") long id, Model model){
+        model.addAttribute("list", listRepository.findById(id).get());
+        return "show";
+    }
+    @RequestMapping("/update/{id}")
+    public String updateList(@PathVariable("id") long id, Model model){
+        model.addAttribute("list", listRepository.findById(id).get());
+        return "listform";
+    }
+    @RequestMapping("/delete/{id}")
+    public String delList(@PathVariable("id") long id){
+        listRepository.deleteById(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/com/example/todolist/List.java
@@ -0,0 +1,51 @@
+package com.example.todolist;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class List {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @NotNull
+    private String taskName, dueDate, priority;
+
+    public List() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTaskName() {
+        return taskName;
+    }
+
+    public void setTaskName(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(String dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+}
